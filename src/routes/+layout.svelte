48f69de0b09c89f73b517028daa41aa3a7d24df2
othavioquiliao <othavioaqa@gmail.com>
2024-01-03 17:12:41 -0300

    Add @vercel/speed-insights dependency and inject it in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-	import Header from '$lib/assets/components/Header.svelte';
-
+	import "../app.postcss";
+	import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+	import Header from "$lib/assets/components/Header.svelte";
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+	injectSpeedInsights();
 	// Highlight JS
-	import hljs from 'highlight.js/lib/core';
-	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
-	import xml from 'highlight.js/lib/languages/xml'; // for HTML
-	import css from 'highlight.js/lib/languages/css';
-	import javascript from 'highlight.js/lib/languages/javascript';
-	import typescript from 'highlight.js/lib/languages/typescript';
+	import hljs from "highlight.js/lib/core";
+	import "highlight.js/styles/github-dark.css";
+	import { storeHighlightJs } from "@skeletonlabs/skeleton";
+	import xml from "highlight.js/lib/languages/xml"; // for HTML
+	import css from "highlight.js/lib/languages/css";
+	import javascript from "highlight.js/lib/languages/javascript";
+	import typescript from "highlight.js/lib/languages/typescript";
 
-	hljs.registerLanguage('xml', xml); // for HTML
-	hljs.registerLanguage('css', css);
-	hljs.registerLanguage('javascript', javascript);
-	hljs.registerLanguage('typescript', typescript);
+	hljs.registerLanguage("xml", xml); // for HTML
+	hljs.registerLanguage("css", css);
+	hljs.registerLanguage("javascript", javascript);
+	hljs.registerLanguage("typescript", typescript);
 	storeHighlightJs.set(hljs);
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from "@floating-ui/dom";
+	import { storePopup } from "@skeletonlabs/skeleton";
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
 	// tenho que testar isso:
 
-	import type { ComponentEvents } from 'svelte';
+	import type { ComponentEvents } from "svelte";
 
-	function scrollHandler(event: ComponentEvents<AppShell>['scroll']) {
+	function scrollHandler(event: ComponentEvents<AppShell>["scroll"]) {
 		console.log(event.currentTarget.scrollTop);
 	}
 </script>
