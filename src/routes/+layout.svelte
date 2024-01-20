512cb10211626d383c43ec04028e814f5e1b360a
othavioquiliao <othavioaqa@gmail.com>
2024-01-20 16:03:30 -0300

    up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, Toast } from '@skeletonlabs/skeleton';
+	import Header from '$lib/assets/components/Header.svelte';
+	import Footer from '$lib/assets/components/Footer.svelte';
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	// Skeleton Stores
+	// import { initializeStores } from '@skeletonlabs/skeleton';
+	// initializeStores();
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	// Componente do Blip Chat
+	// import BlipChat from "$lib/assets/components/BlipChat.svelte";
+</script>
+
+<Toast />
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<Header />
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<main class="relative">
+		<slot />
+	</main>
+	<svelte:fragment slot="pageFooter">
+		<Footer />
+	</svelte:fragment>
+</AppShell>
---
+++ src/routes/sobre-nos/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import LocationCard from "$lib/assets/components/Filiais/LocationCard.svelte";
+    import { listaFiliais } from "$lib/assets/components/Filiais/listaFiliais";
+</script>
+
+<section class="px-10 pt-10 space-y-5">
+    <h1 class="h1 text-center font-bold gradient-heading fontSpace">Bem vindo!</h1>
+    <h3 class="h3">Um pouco sobre a nossa historia...</h3>
+
+    <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis qui quam dolorem
+        aspernatur? Ab, voluptatibus ratione? Facere omnis iusto pariatur recusandae dolor cumque
+        accusamus explicabo consectetur officiis magni! Cupiditate, dolor! Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Earum tempora ut at, id
+    </article>
+
+    <h3 class="h3">E foi assim que nos...</h3>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis qui quam dolorem
+        aspernatur? Ab, voluptatibus ratione? Facere omnis iusto pariatur recusandae dolor cumque
+        accusamus explicabo consectetur officiis magni! Cupiditate, dolor! Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Earum tempora ut at, id repellat rem odio iste esse
+        nostrum inventore a, illum deserunt. Accusantium at necessitatibus temporibus reprehenderit
+        totam quasi?
+    </p>
+</section>
+<section class="px-10 py-10 space-y-5">
+    <h2 class="h5 md:h2 font-semibold gradient-heading">
+        Encontre a HIMARTE mais proxima de você!
+    </h2>
+    <div class="flex flex-col gap-5 items-center w-full">
+        <div class="flex flex-col md:flex-row md:flex-wrap w-full gap-5 md:gap-0">
+            {#each listaFiliais as location}
+                <LocationCard {location} />
+            {/each}
+        </div>
+    </div>
+</section>
