74e749a6b58a80632fc36f233acb5d268f90315b
Othavio Quiliao <othavioaqa@gmail.com>
2024-01-26 09:44:15 -0300

    Adiciona arquivos e configurações para o adaptador Bun

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
