277a03503e0dc36fb38983d8e05a2ef4cc9e0041
JacobsenNando <fernandofjlr@gmail.com>
2024-01-30 15:59:05 -0300

    UpSobreNos

--- src/routes/sobre-nos/+page.svelte
+++ src/routes/sobre-nos/+page.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-    import LocationCard from "$lib/assets/components/Filiais/LocationCard.svelte";
-    import { listaFiliais } from "$lib/assets/components/Filiais/listaFiliais";
+	import LocationCard from '$lib/assets/components/Filiais/LocationCard.svelte';
+	import { listaFiliais } from '$lib/assets/components/Filiais/listaFiliais';
 </script>
 
 <section class="px-10 pt-10 space-y-5">
-    <h1 class="h1 text-center font-bold gradient-heading fontSpace">Bem vindo!</h1>
-    <h3 class="h3">Um pouco sobre a nossa historia...</h3>
+	<h1 class="h1 text-center font-bold gradient-heading fontSpace">Bem vindo!</h1>
+	<h3 class="h3">Um pouco sobre a nós...</h3>
 
-    <article>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis qui quam dolorem
-        aspernatur? Ab, voluptatibus ratione? Facere omnis iusto pariatur recusandae dolor cumque
-        accusamus explicabo consectetur officiis magni! Cupiditate, dolor! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Earum tempora ut at, id
-    </article>
+	<article>
+		A Himarte Net, anteriormente conhecida como Infotec, é uma empresa pioneira e bem-sucedida que
+		teve suas raízes em Candelária, onde foi fundada em 2002 pelos visionários Márcia Martin e Hiuri
+		Ilha. Especializada inicialmente em serviços de informática, a empresa rapidamente se destacou
+		ao se tornar a primeira a fornecer internet via rádio em diversas localidades, marcando o início
+		de uma jornada que a levaria a se posicionar como líder no setor de telecomunicações.
+	</article>
 
-    <h3 class="h3">E foi assim que nos...</h3>
-
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis qui quam dolorem
-        aspernatur? Ab, voluptatibus ratione? Facere omnis iusto pariatur recusandae dolor cumque
-        accusamus explicabo consectetur officiis magni! Cupiditate, dolor! Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Earum tempora ut at, id repellat rem odio iste esse
-        nostrum inventore a, illum deserunt. Accusantium at necessitatibus temporibus reprehenderit
-        totam quasi?
-    </p>
+	<h3 class="h3">Nossa história...</h3>
+	<p>
+		Ao longo dos anos, a Himarte Net evoluiu continuamente, incorporando inovações tecnológicas como
+		a adoção da tecnologia 5.8 em 2010 e a transição para fibra óptica em 2017. Essas mudanças não
+		apenas revolucionaram o mercado, mas também possibilitaram à empresa expandir sua presença para
+		diferentes municípios. Atualmente, com uma equipe de 57 colaboradores, a Himarte Net oferece uma
+		variedade de planos residenciais e empresariais, juntamente com serviços de transporte de dados,
+		destacando-se como uma referência confiável no fornecimento de internet de alta qualidade.
+	</p>
 </section>
 <section class="px-10 py-10 space-y-5">
-    <h2 class="h5 md:h2 font-semibold gradient-heading">
-        Encontre a HIMARTE mais proxima de você!
-    </h2>
-    <div class="flex flex-col gap-5 items-center w-full">
-        <div class="flex flex-col md:flex-row md:flex-wrap w-full gap-5 md:gap-0">
-            {#each listaFiliais as location}
-                <LocationCard {location} />
-            {/each}
-        </div>
-    </div>
+	<h2 class="h5 md:h2 font-semibold gradient-heading">Encontre a HIMARTE mais proxima de você!</h2>
+	<div class="flex flex-col gap-5 items-center w-full">
+		<div class="flex flex-col md:flex-row md:flex-wrap w-full gap-5 md:gap-0">
+			{#each listaFiliais as location}
+				<LocationCard {location} />
+			{/each}
+		</div>
+	</div>
 </section>
