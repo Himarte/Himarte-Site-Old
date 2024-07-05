8bce9e7bf4eb0d14a572a21fe9902de09aa566d9
Othavio Quiliao <othavioaqa@gmail.com>
2024-07-05 11:38:31 -0300

    LinkTree finalizado

--- src/routes/linktree/+page.svelte
+++ src/routes/linktree/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import astroColorido from '$lib/assets/img/extras/astro-costas.jpg';
-	import appStore from '$lib/assets/img/logos/appleStore.png';
-	import googlePlay from '$lib/assets/img/logos/googleStore.png';
-	import mobileFrame from '$lib/assets/img/extras/mobile.png';
-	import instagramIcon from '$lib/assets/img/extras/instaIcon.png';
-	import facebookIcon from '$lib/assets/img/extras/facebookIcone.png';
-	import linkedinIcon from '$lib/assets/img/extras/linkedinIcon.png';
-	import whatsappIcon from '$lib/assets/img/extras/whatsIcon.png';
+	import astroColorido from '$lib/assets/img/extras/astro-costas.webp';
+	import appStore from '$lib/assets/img/logos/appleStore.webp';
+	import googlePlay from '$lib/assets/img/logos/googleStore.webp';
+	import mobileFrame from '$lib/assets/img/extras/mobile.webp';
+	import instagramIcon from '$lib/assets/img/extras/instaIcon.webp';
+	import facebookIcon from '$lib/assets/img/extras/facebookIcone.webp';
+	import linkedinIcon from '$lib/assets/img/extras/linkedinIcon.webp';
+	import whatsappIcon from '$lib/assets/img/extras/whatsIcon.webp';
 </script>
 
 <div
@@ -21,22 +21,49 @@
 		<img
 			src={astroColorido}
 			alt="Astronauta"
-			class=" absolute object-cover h-full w-full mix-blend-difference !rounded-3xl aspect-auto blur-[3px]"
+			class="absolute object-cover h-full w-full mix-blend-difference !rounded-3xl aspect-auto blur-[3px]"
 		/>
 		<nav
 			class="flex flex-col justify-center absolute items-center z-30 w-full h-full !bg-transparent gap-5"
 		>
 			<div class="w-5/6 flex items-center justify-center gap-3 mb-10">
-				<a href="Qualquer" class="w-1/2 card-hover"><img src={appStore} alt="Apple Store" /></a>
-				<a href="qualquer" class="w-1/2 card-hover"><img src={googlePlay} alt="Google Play" /></a>
+				<a
+					target="_blank"
+					href="https://apps.apple.com/ro/app/himarte-net/id6448640271"
+					class="w-1/2 card-hover"
+				>
+					<img src={appStore} alt="Apple Store" />
+				</a>
+				<a
+					href="https://play.google.com/store/apps/details?id=br.com.portal.himarte&hl=pt_BR&gl=US&pli=1"
+					class="w-1/2 card-hover"
+				>
+					<img src={googlePlay} alt="Google Play" />
+				</a>
 			</div>
 			<div class="w-2/5 gap-10 flex justify-center items-center mb-5">
-				<a href="qualquer" class="w-1/2 card-hover"><img src={facebookIcon} alt="Facebook" /></a>
-				<a href="qualquer" class="w-1/2 card-hover"><img src={instagramIcon} alt="Instagram" /></a>
+				<a target="_blank" href="https://www.facebook.com/himarte.net/" class="w-1/2 card-hover">
+					<img src={facebookIcon} alt="Facebook" />
+				</a>
+				<a target="_blank" href="https://www.instagram.com/himarte_net/" class="w-1/2 card-hover">
+					<img src={instagramIcon} alt="Instagram" />
+				</a>
 			</div>
 			<div class="w-2/5 gap-10 flex justify-center items-center">
-				<a href="qualquer" class="w-1/2 card-hover"><img src={linkedinIcon} alt="Linkedin" /></a>
-				<a href="qualquer" class="w-1/2 card-hover"><img src={whatsappIcon} alt="Whatsapp" /></a>
+				<a
+					target="_blank"
+					href="https://www.linkedin.com/company/himartenet/"
+					class="w-1/2 card-hover"
+				>
+					<img src={linkedinIcon} alt="Linkedin" />
+				</a>
+				<a
+					target="_blank"
+					href="https://wa.me/555130568251?text=Ola!%20Tudo%20bem?"
+					class="w-1/2 card-hover"
+				>
+					<img src={whatsappIcon} alt="Whatsapp" />
+				</a>
 			</div>
 		</nav>
 	</div>
